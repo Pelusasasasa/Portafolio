555fa5b5263f93fe8b0107ba1f8b459cb8241a81
Pelusa <agustinlorenzatto@gmail.com>
2025-03-29 08:59:59 -0300

    Empenzando con la configuracion del reducer

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,18 @@
 import { StrictMode } from 'react';
-import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { PortFolio } from './PortFolio.jsx'
 
 import './index.css'
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { store } from './reducer';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <PortFolio />
+    <Provider store={store}>
+        <BrowserRouter>
+          <PortFolio />
+        </BrowserRouter>
+      </Provider>
   </StrictMode>,
 )
